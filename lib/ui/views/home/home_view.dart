2750e5f9b3546765a2e28aaf8cddd1d023e4abdf
Priyank <priyupadhyay1994@gmail.com>
2021-05-16 15:32:33 +0530

    modified end-text design

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -14,72 +14,74 @@ class HomeView extends StatelessWidget {
     return ViewModelBuilder<HomeViewModel>.reactive(
       onModelReady: (model) => model.getInitalStories(),
       builder: (context, model, child) => Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            title: Text(
-              "Top Stories",
-              style: h3.copyWith(color: TextColorDark),
-            ),
-            leading: BackButton(
-              color: TextColorDark,
-            ),
-            actions: [
-              RotatedWidget(
-                widgetToAnimate: IconButton(
-                  icon: Icon(
-                    Icons.cached_rounded,
-                    color: TextColorDark,
-                  ),
-                  onPressed: () {
-                    model.getStoriesAndCache();
-                    _animationSyncButtonController?.rotate();
-                  },
-                ),
-                animationCallback: (animationCallback) {
-                  _animationSyncButtonController = animationCallback;
-                },
-              )
-            ],
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text(
+            "Top Stories",
+            style: h3.copyWith(color: TextColorDark),
           ),
-          body: model.stories.length == 0
-              ? model.loader
-              : Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: NotificationListener<ScrollNotification>(
-                    onNotification: (ScrollNotification scrollInfo) {
-                      if (scrollInfo.metrics.pixels ==
-                          scrollInfo.metrics.maxScrollExtent) {
-                        model.getNext();
-                      }
-                      return true;
+          leading: BackButton(
+            color: TextColorDark,
+          ),
+          actions: [
+            RotatedWidget(
+              widgetToAnimate: IconButton(
+                icon: Icon(
+                  Icons.cached_rounded,
+                  color: TextColorDark,
+                ),
+                onPressed: () {
+                  model.getStoriesAndCache();
+                  _animationSyncButtonController?.rotate();
+                },
+              ),
+              animationCallback: (animationCallback) {
+                _animationSyncButtonController = animationCallback;
+              },
+            )
+          ],
+        ),
+        body: model.stories.length == 0
+            ? model.loader
+            : Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: NotificationListener<ScrollNotification>(
+                  onNotification: (ScrollNotification scrollInfo) {
+                    if (scrollInfo.metrics.pixels ==
+                        scrollInfo.metrics.maxScrollExtent) {
+                      model.getNext();
+                    }
+                    return true;
+                  },
+                  child: ListView.builder(
+                    itemCount: model.stories.length,
+                    itemBuilder: (_, index) {
+                      try {
+                        _animationSyncButtonController?.stop();
+                      } catch (exception) {}
+                      return ListTile(
+                        onTap: () {
+                          model.openStory(model.stories[index].url);
+                        },
+                        title: Text(model.stories[index].title,
+                            style: h4.copyWith(color: TextColorDark)),
+                        subtitle: Text(model.stories[index].by),
+                      );
                     },
-                    child: ListView.builder(
-                      itemCount: model.stories.length,
-                      itemBuilder: (_, index) {
-                        try {
-                          _animationSyncButtonController?.stop();
-                        } catch (exception) {}
-                        return ListTile(
-                          onTap: () {
-                            model.openStory(model.stories[index].url);
-                          },
-                          title: Text(model.stories[index].title,
-                              style: h4.copyWith(color: TextColorDark)),
-                          subtitle: Text(model.stories[index].by),
-                        );
-                      },
-                    ),
                   ),
                 ),
-          bottomNavigationBar: model.showBottomBarText
-              ? Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Loading more stories ..."),
-                  ],
-                )
-              : null),
+              ),
+        bottomNavigationBar: model.showBottomBarText
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Loading more stories ...",
+                      style: h4.copyWith(color: TextColorDark, fontSize: 12)),
+                ],
+              )
+            : null,
+      ),
       viewModelBuilder: () => HomeViewModel(),
     );
   }
